a82f6cfe2ba8bb22e96ce15c76f98d99443a56d4
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-10-13 22:12:40 -0400

    added winning functions

--- script.js
+++ script.js
@@ -1,62 +1,72 @@
-let playerText=document.getElementById('playerText');
-let restartBtn=document.getElementById('restartBtn');
-let boxes=Array.from(document.getElementsByClassName('box'));
+let playerText = document.getElementById("playerText");
+let restartBtn = document.getElementById("restartBtn");
+let boxes = Array.from(document.getElementsByClassName("box"));
 // initializes the variables that will be used to start the game
 
-const O_TEXT="O";
-const X_TEXT="X";
-let currentPlayer=X_TEXT;
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue(
+  "--winning-blocks"
+);
+const O_TEXT = "O";
+const X_TEXT = "X";
+let currentPlayer = X_TEXT;
 // keeps track of the spaces being clicked
-let spaces=Array(9).fill(null);
+let spaces = Array(9).fill(null);
 // Creates functionality for the game board
 
-const startGame=() => {
-    boxes.forEach(box => box.addEventListener('click',boxClicked));
-    
-}
+const startGame = () => {
+  boxes.forEach((box) => box.addEventListener("click", boxClicked));
+};
 // function to start game and to loop through the boxes.
 
-function boxClicked(e){
-    const id = e.target.id;
-    if(!spaces[id]){
-        spaces[id] =currentPlayer
-        e.target.innerText = currentPlayer
-        if(playerHasWon()){
-            playerText = `${currentPlayer} Is the Winner!`;
-
-        }
-
-        currentPlayer = currentPlayer ==X_TEXT ? O_TEXT: X_TEXT
-
+function boxClicked(e) {
+  const id = e.target.id;
+  if (!spaces[id]) {
+    spaces[id] = currentPlayer;
+    e.target.innerText = currentPlayer;
+    if (playerHasWon() !== false) {
+      playerText.innerHTML = `${currentPlayer} Is the Winner!`;
+      let winning_block =playerHasWon();
+    
+      winning_block.map(box => boxes[box].style.backgroundColor=winnerIndicator)
+      return;
     }
+
+    currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+  }
 }
 
-const winningCombos =[
-    [0,1,2], 
-    [3,4,5], 
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 function playerHasWon() {
+  for (const condition of winningCombos) {
+    let [a, b, c] = condition;
 
+    if (spaces[a] && spaces[a] == spaces[b] && spaces[a] == spaces[c])
+      return [a, b, c];
+  }
+  return false;
 }
 
-restartBtn.addEventListener('click',restart)
+restartBtn.addEventListener("click", restart);
 
-function restart(){
-    spaces.fill(null)
-    boxes.forEach(box=>{
-        box.innerText=''
-        box.getElementsByClassName.backgroundColor=''
-    })
+function restart() {
+  spaces.fill(null);
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.getElementsByClassName.backgroundColor = "";
+  });
 
-playerText = 'Tic Tac Toe'
+  playerText.innerHTML = "Tic Tac Toe";
 
-    currentPlayer = X_TEXT
+  currentPlayer = X_TEXT;
 }
 
 startGame();
